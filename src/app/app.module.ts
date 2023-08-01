00126dbe653b88373ce4514be7a8c03f27d15327
frizzoana <frizzoanapaula04@gmail.com>
2023-08-01 13:39:11 -0300

    cabeçalho, imoveis e favoritos

---
+++ src/app/app.module.ts
@@ -0,0 +1,39 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations'; 
+import { AppRoutingModule } from './app-routing.module';
+
+//MEUS BLOCOS DE CODIGO
+import { AppComponent } from './app.component';
+import { CabecalhoComponent } from './cabecalho/cabecalho.component';
+import { ContatoComponent } from './contato/contato.component';
+import { QuemSomosComponent } from './quem-somos/quem-somos.component';
+
+
+//MATERIAL UI
+import {MatIconModule} from '@angular/material/icon';
+import {MatButtonModule} from '@angular/material/button';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import { DetalhesComponent } from './detalhes/detalhes.component';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    QuemSomosComponent,
+    ContatoComponent,
+    CabecalhoComponent,
+    DetalhesComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatIconModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
